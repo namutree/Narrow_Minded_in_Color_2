41bf497d15a79dff2f14703d99743c468ad808fb
John N Choi <johnchoi@172-16-206-45.DYNAPOOL.NYU.EDU>
2015-08-06 22:14:13 -0400

    new UX3

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var w = screen.width;
-	h= 48*200//800
+	h= 800,
 	imgW = 64,
 	imgH = 48;
 var sSub =[],
@@ -12,7 +12,11 @@ console.log(w);
 //id,Original_id,Sel_r,Sel_g,Sel_b,sex,Ori_r,Ori_g,Ori_b,Sel_hue,Sel_sat,Sel_bri,Ori_hue,Ori_sat,Ori_bri,Bri_sub, bri_sub_order,Sat_sub,sat_sub_order
 d3.csv("data_final_3.csv", function(error, data){
 	var n = data.length;
-	console.log(n);
+	//console.log(n);
+	var imgSmW = Math.floor((w-imgW*2)/n);
+	if (imgSmW < 2) imgSmW =2;
+	else if (imgSmW > 6) imgSmW = 6;
+	console.log(imgSmW, (w)/n);
 
 	if (error) throw error;
 	data.forEach(function(d){
@@ -28,14 +32,12 @@ d3.csv("data_final_3.csv", function(error, data){
 
 	var xPos = d3.scale.linear()
     	.domain([0, +n-1])
-    	.range([0 , w- imgW-1])
-    var yPos = d3.scale.linear()
-    	.domain([0,+n-1])
-    	.range([10 ,h-64])
+    	.range([0 +imgW/2 , w- imgW/2])
+
 
     //creating uppest level svg
   	var uppest = d3.select('#chart').append('svg')
-		.style('background', 'rgb(255,230,220)')//)
+		.style('background', 'rgb(230,230,230)')
 		.attr('width', w)
 		.attr('height', h)
 
@@ -73,7 +75,7 @@ d3.csv("data_final_3.csv", function(error, data){
 
 	var test = eachRow.append('rect')
 		.attr('class', 'ImgBox')
-		.attr('width', 5)
+		.attr('width', imgSmW)
 		.attr('height', imgH)
 		.attr('x', function(d){
 			//console.log(xPos(+d.id));
@@ -88,29 +90,73 @@ d3.csv("data_final_3.csv", function(error, data){
 	d3.selectAll('.rects')
 		.on("mouseover", function(data){
 
+			//img location
 			d3.selectAll('.ImgBox')
-				.transition()
+				.transition()//.duration(100)
 				.attr('x', function(d){
-					if (+d.id > +data.id){
-						console.log('dd');
-						return xPos(+d.id) + imgW;
-					}else {
-						return xPos(+d.id);
+					if (+d.id > +data.id) {
+						return xPos(+d.id) + imgW/2-imgSmW;
+					}else if (+d.id < +data.id) {
+						return xPos(+d.id) - imgW/2;
+					}
+					else {
+						return xPos(+d.id)- imgW/2;
 					}
 				})
-			d3.select('#p'+data.Original_id).transition().attr('x', xPos(+data.id))
-			d3.select('#n'+data.id).select('rect').transition().attr('width', imgW);
+			d3.selectAll('pattern')
+				.transition()
+				.attr('x', function(d){
+					if (+d.id == +data.id) {
+						return xPos(+data.id)- imgW/2;
+					}
+					else {
+						return xPos(+d.id) - imgW;
+					}
+				})
+			//pattern location
+			// d3.select('#p'+data.Original_id)
+			// 	.transition().duration(100)
+			// 	.attr('x', xPos(+data.id)- imgW/2)
+			//image size
+			d3.select('#n'+data.id).select('rect')
+				// .transition().duration(100)
+				.attr('width', imgW);
 		})
 		.on('mouseout', function(data){
 			d3.selectAll('.ImgBox')
-				.transition()
+				.transition()//.duration(100)
 				.attr('x', function(d){
 					return xPos(+d.id);
 				})
-			d3.select('#p'+data.Original_id).transition().attr('x', (xPos(+data.id) + imgW/2));
-			d3.select(this).select('rect').transition().attr('width', 5);
+			
+			d3.selectAll('pattern')
+				.transition()
+				.attr('x', function(d){
+					return xPos(+d.id) + imgW/2;
+					//(xPos(+data.id) + imgW/2)
+				});
+
+			// d3.select('#p'+data.Original_id)
+			// .transition().duration(100)
+			// 	.attr('x', (xPos(+data.id) + imgW/2));
+			
+			d3.select(this).select('rect')
+			// .transition().duration(100)
+				.attr('width', imgSmW);
 		})
 
+	//  //check the statistic every 0.3 sec!!
+	// var IntervalTest = setInterval(function(){
+	//     	//console.log('yoyo');
+	//     	d3.selectAll('.ImgBox')
+	// 			.transition()
+	// 			.attr('x', function(d){
+	// 				return xPos(+d.id);
+	// 			})
+	// 		d3.select('#p'+data.Original_id).transition().attr('x', (xPos(+data.id) + imgW/2));
+	// 		//d3.select(this).select('rect').transition().attr('width', 5);
+
+	//     },300)
 })
 
 
